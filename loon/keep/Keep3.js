5a48522a8851939bc53ad6f9c39dde05f1563f9f
1amfine2333 <i_am_fine_2333@qq.com>
2023-04-04 17:47:40 +0800

    Update Keep3.js

--- loon/keep/Keep3.js
+++ loon/keep/Keep3.js
@@ -1,16 +1,1 @@
-if (-1 != $request.url.indexOf("arke-webapp/v3/home/prime/page")) {
-	let e = JSON.parse($response.body.replace(/\"memberStatus":\d+/g, '"memberStatus":1')
-		.replace(/\"buttonText":".*?"/g, '"buttonText":"永久会员"')
-		.replace(/\"hasPaid\":\w+/g, '"hasPaid":true')
-		.replace(/\"downLoadAll\":\w+/g, '"downLoadAll":true')
-		.replace(/\"videoTime\":\d+/g, '\"videoTime\":3000')
-		.replace(/\"startEnable\":\w+/g, '"startEnable":true')
-		.replace(/\"memberStatus\":\d+/g, '"memberStatus":1')
-		.replace(/\"preview\":\w+/g, '"preview":true')
-		.replace(/\"errorCode\":\d+/g, '"errorCode":0')
-		.replace(/\"status\":\w+/g, '\"status\":true'));
-	e.data.moduleItems.moduleMap.primeEntrySmallNewV3.primeUserInfo.intros = []$done({
-		body: JSON.stringify(e)
-	})
-}
-else $done();
+
